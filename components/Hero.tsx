b45e21715ca33b856d6b720129f6821442c5e0ef
Ian V. Tatiana <ian.tatiana.v@gmail.com>
2026-05-27 00:17:32 +0300

    Update content, hero animations, icons and mobile fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,6 +12,7 @@ export default function Hero() {
         display: "flex",
         flexDirection: "column",
         position: "relative",
+        overflow: "hidden",
       }}
     >
       {/* Animated background */}
